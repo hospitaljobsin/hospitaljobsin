8199a896fec2f00c1f87ba3fd17a24b36f1affd5
Aryan Iyappan <aryaniyappan2006@gmail.com>
2024-12-01 17:18:36 +0530

    add terraform skeleton

---
+++ infrastructure/variables.tf
@@ -0,0 +1,18 @@
+
+variable "app_name" {
+  type    = string
+  default = "Hospital Job Board"
+}
+
+variable "resource_prefix" {
+  type        = string
+  description = "The prefix to use for all resources."
+  default     = "medicaljobs"
+}
+
+
+variable "aws_region" {
+  type        = string
+  description = "Location for all AWS resources."
+  default     = "us-east-1"
+}
