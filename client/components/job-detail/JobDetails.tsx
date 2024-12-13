ad08ca8c890962e47f1cf11b39fbbc6e1c4efe79
Aryan Iyappan <aryaniyappan2006@gmail.com>
2024-12-13 21:13:19 +0530

    refine job details page

--- client/components/job-detail/JobDetails.tsx
+++ client/components/job-detail/JobDetails.tsx
@@ -4,6 +4,7 @@ import {
   Button,
   Card,
   CardBody,
+  CardFooter,
   CardHeader,
   Chip,
   Divider,
@@ -99,20 +100,16 @@ export default function JobDetails({ job }: { job: JobDetailsFragment$key }) {
       <Card fullWidth className="p-6">
         <CardHeader>
           <div className="flex w-full justify-between gap-4 items-center">
-            <div className="flex items-center gap-4">
-              <Avatar
-                name={data.company?.name}
-                src={data.company?.logoUrl || undefined}
-                size="lg"
-              />
+              
               <div className="flex flex-col gap-2 items-start">
                 <h4 className="text-xl font-medium">{data.title}</h4>
+
                 <p className="text-md font-normal text-foreground-500">
                   {data.company?.name}
                 </p>
-              </div>
             </div>
-            <Button size="lg">Apply now</Button>
+            <div className="flex items-center gap-8">
+            <Button size="lg" variant="bordered">Save</Button><Button size="lg">Apply now</Button></div>
           </div>
         </CardHeader>
         <CardBody className="flex flex-col gap-6 w-full">
@@ -142,48 +139,24 @@ export default function JobDetails({ job }: { job: JobDetailsFragment$key }) {
       </Card>
 
       {/* Job and Company Details */}
-      <div className="grid gap-4 grid-cols-2">
-        <Card>
-          <CardHeader>
-            <h3 className="text-lg font-semibold text-secondary">
-              Key Details
-            </h3>
-          </CardHeader>
-          <Divider />
-          <CardBody>
-            <ul className="text-default-500 space-y-2">
-              <li>
-                <strong>Location:</strong>{" "}
-                {`${data.address.city}, ${data.address.state}`}
-              </li>
-              <li>
-                <strong>Salary:</strong> {salaryRange}
-              </li>
-              <li>
-                <strong>Closing Date:</strong> {formattedExpiresAt}
-              </li>
-              <li>
-                <strong>Experience:</strong> {experienceRange}
-              </li>
-            </ul>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardHeader>
-            <h3 className="text-lg font-semibold text-secondary">
-              About the Company
-            </h3>
-          </CardHeader>
-          <Divider />
-          <CardBody>
-            <div className="flex gap-6 items-center">
-              <Avatar
+       
+        <Card fullWidth className="p-6">
+          <CardHeader className="flex gap-4 items-center">
+          <Avatar
                 name={data.company?.name}
+                src={data.company?.logoUrl || undefined}
                 size="lg"
-                className="h-24 w-24"
               />
+            <h3 className="text-md font-medium text-secondary">
+             {data.company?.name}
+            </h3>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+          <p className="text-default-500">{data.company?.description}</p>
+
               <div>
-                <p className="text-default-500">{data.company?.description}</p>
+                
                 <ul className="text-default-500 space-y-2 mt-2">
                   <li>
                     <strong>Address:</strong>{" "}
@@ -206,14 +179,12 @@ export default function JobDetails({ job }: { job: JobDetailsFragment$key }) {
                     <strong>Phone:</strong> {data.company?.phone}
                   </li>
                 </ul>
-              </div>
             </div>
           </CardBody>
         </Card>
-      </div>
 
       {/* Job Description */}
-      <Card>
+      <Card className="p-6" fullWidth>
         <CardHeader>
           <h3 className="text-lg font-semibold text-secondary">
             About the Job
@@ -224,18 +195,9 @@ export default function JobDetails({ job }: { job: JobDetailsFragment$key }) {
           <ReactMarkdown className="prose dark:prose-invert prose-h2:text-sm">
             {data.description}
           </ReactMarkdown>
+          
         </CardBody>
-      </Card>
-
-      {/* Skills */}
-      <Card>
-        <CardHeader>
-          <h3 className="text-lg font-semibold text-secondary">
-            Required Skills
-          </h3>
-        </CardHeader>
-        <Divider />
-        <CardBody>
+        <CardFooter>
         <div className="flex flex-wrap gap-4 mt-2 w-full">
             {data.skills.map((skill, index) => (
               <Chip variant="flat" key={index}>
@@ -243,7 +205,7 @@ export default function JobDetails({ job }: { job: JobDetailsFragment$key }) {
               </Chip>
             ))}
           </div>
-        </CardBody>
+        </CardFooter>
       </Card>
     </div>
   );
