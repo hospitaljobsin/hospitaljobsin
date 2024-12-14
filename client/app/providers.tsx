de4c7fa9f8126eb7f999d6f6baba0c08e5b8f7d2
Aryan Iyappan <aryaniyappan2006@gmail.com>
2024-12-14 09:52:19 +0530

    enable client side navigation with nextui

--- client/app/providers.tsx
+++ client/app/providers.tsx
@@ -3,16 +3,18 @@ import { getCurrentEnvironment } from "@/lib/relay/environment";
 import { NextUIProvider } from "@nextui-org/react";
 import { AppProgressBar } from "next-nprogress-bar";
 import { ThemeProvider } from "next-themes";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { RelayEnvironmentProvider } from "react-relay";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
   const [environment] = useState(() => {
     return getCurrentEnvironment();
   });
 
   return (
-    <NextUIProvider>
+    <NextUIProvider navigate={router.push}>
       <ThemeProvider attribute="class" forcedTheme="light" enableSystem={false}>
         <RelayEnvironmentProvider environment={environment}>
           {children}
