0a3fd133d0fc31ae287e166fc516893478575426
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-01-10 14:22:59 +0530

    show subfields on profile personal details display

--- server/app/base/models.py
+++ server/app/base/models.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel
 
 
 class Address(BaseModel):
-    line1: str | None
-    line2: str | None
-    city: str | None
-    state: str | None
-    country: str | None
-    pincode: str | None
+    line1: str | None = None
+    line2: str | None = None
+    city: str | None = None
+    state: str | None = None
+    country: str | None = None
+    pincode: str | None = None
