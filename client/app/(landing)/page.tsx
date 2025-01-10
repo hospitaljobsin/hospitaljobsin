79ba4119698ea97da37b352798488fbc4df0c016
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-01-10 21:00:55 +0530

    branding shift to locum jobs

--- client/app/(landing)/page.tsx
+++ client/app/(landing)/page.tsx
@@ -8,10 +8,16 @@ export default function Landing() {
 	return (
 		<>
 			<div className="bg-gray-100 w-full">
-				<div className="py-24 px-5 w-full h-full flex flex-col gap-8 text-center max-w-5xl mx-auto">
+				<div className="py-28 px-5 w-full h-full flex flex-col gap-8 text-center max-w-5xl mx-auto">
+					<h2 className="mx-auto text-xl text-balance text-foreground-500">
+						Connecting you to top locum opportunities in healthcare.
+					</h2>
 					<h1 className="text-5xl font-medium text-balance">
-						Find Your Perfect Fit in Medicine: Explore Top Healthcare Careers
+						Your Trusted Locum Job Board
 					</h1>
+					<h2 className="mx-auto text-xl text-balance text-foreground-500">
+						Find <i>your</i> right role, right now.
+					</h2>
 				</div>
 			</div>
 			<div className="py-8 w-full h-full flex flex-col gap-8 max-w-5xl mx-auto">
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,4 @@
-import { APP_NAME } from "@/lib/constants";
+import { APP_NAME, APP_TAGLINE } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
 		template: `%s | ${APP_NAME}`,
 		default: APP_NAME,
 	},
+	description: APP_TAGLINE,
 };
 
 export default async function RootLayout({
