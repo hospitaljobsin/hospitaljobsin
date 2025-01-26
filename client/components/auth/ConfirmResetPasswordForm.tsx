fb8654e8b39b2f202050c6cb9ce3a0fb9001d080
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-01-26 17:19:03 +0530

    revert reset password form deletion
    remove aws-amplify code

---
+++ client/components/auth/ConfirmResetPasswordForm.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import links from "@/lib/links";
+import { Button, Card, CardBody, CardHeader, Input } from "@heroui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { useRouter } from "next-nprogress-bar";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useMutation } from "react-relay";
+import { graphql } from "relay-runtime";
+import { z } from "zod";
+import type { ConfirmResetPasswordFormMutation as ConfirmResetPasswordFormMutationType } from "./__generated__/ConfirmResetPasswordFormMutation.graphql";
+
+const ConfirmResetPasswordFormMutation = graphql`
+  mutation ConfirmResetPasswordFormMutation($email: String!, $passwordResetToken: String!, $newPassword: String!) {
+	resetPassword(email: $email, passwordResetToken: $passwordResetToken, newPassword: $newPassword) {
+	  __typename
+	  ... on Account {
+		...AuthDropdownFragment
+	  }
+	  ... on InvalidPasswordResetTokenError {
+		message
+	  }
+	}
+  }
+`;
+
+const confirmResetPasswordSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(6),
+});
+
+export default function ConfirmResetPasswordForm() {
+	const router = useRouter();
+	const params = useParams<{ token: string }>();
+
+	const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+	const [commitMutation, isMutationInFlight] =
+		useMutation<ConfirmResetPasswordFormMutationType>(
+			ConfirmResetPasswordFormMutation,
+		);
+	const [errorMessage, setErrorMessage] = useState<string | null>(null);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm<z.infer<typeof confirmResetPasswordSchema>>({
+		resolver: zodResolver(confirmResetPasswordSchema),
+	});
+
+	function onSubmit(values: z.infer<typeof confirmResetPasswordSchema>) {
+		commitMutation({
+			variables: {
+				email: values.email,
+				passwordResetToken: params.token,
+				newPassword: values.password,
+			},
+			onCompleted(data) {
+				if (
+					data.resetPassword.__typename === "InvalidPasswordResetTokenError"
+				) {
+					setErrorMessage(data.resetPassword.message);
+				} else {
+					router.replace(links.landing);
+				}
+			},
+		});
+	}
+	return (
+		<Card shadow="sm">
+			<CardHeader>
+				<h1 className={"w-full text-center text-2xl"}>Reset your password</h1>
+			</CardHeader>
+			<CardBody>
+				<form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
+					<div className="flex-1 rounded-lg px-6 pb-4 pt-8">
+						<div className="w-full flex flex-col gap-6">
+							<Input
+								id="email"
+								label="Email"
+								placeholder="Enter your email address"
+								type="email"
+								{...register("email")}
+								errorMessage={errors.email?.message}
+								isInvalid={!!errors.email}
+							/>
+							<Input
+								id="password"
+								label="New Password"
+								placeholder="Enter password"
+								type={isPasswordVisible ? "text" : "password"}
+								endContent={
+									<button
+										aria-label="toggle password visibility"
+										className="focus:outline-none"
+										type="button"
+										onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+									>
+										{isPasswordVisible ? (
+											<EyeIcon className="text-2xl text-default-400 pointer-events-none" />
+										) : (
+											<EyeOffIcon className="text-2xl text-default-400 pointer-events-none" />
+										)}
+									</button>
+								}
+								{...register("password")}
+								errorMessage={errors.password?.message}
+								isInvalid={!!errors.password}
+							/>
+							<Button
+								fullWidth
+								isLoading={isSubmitting || isMutationInFlight}
+								type="submit"
+							>
+								Reset Password
+							</Button>
+						</div>
+
+						<div className="flex h-8 items-end space-x-1">
+							<div
+								className="flex h-8 items-end space-x-1"
+								aria-live="polite"
+								aria-atomic="true"
+							>
+								{errorMessage && (
+									<p className="text-sm text-red-500">{errorMessage}</p>
+								)}
+							</div>
+						</div>
+					</div>
+				</form>
+			</CardBody>
+		</Card>
+	);
+}
