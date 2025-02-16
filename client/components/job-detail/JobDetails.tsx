2499fcf9d16a1847f66389c6a0b7a3b111eb10d9
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-02-16 16:07:20 +0530

    make job detail page responsive

--- client/components/job-detail/JobDetails.tsx
+++ client/components/job-detail/JobDetails.tsx
@@ -136,7 +136,7 @@ export default function JobDetails({
 			<p className="text-sm">/ month</p>
 		</div>
 	) : (
-		<div className="flex items-center gap-2 text-md text-foreground-500">
+		<div className="flex items-center gap-2 text-sm text-foreground-500">
 			{currencyIcon(data.currency)}
 			{"Not disclosed"}
 		</div>
@@ -151,7 +151,7 @@ export default function JobDetails({
 			{/* Job Title and Company */}
 			<Card fullWidth className="p-6" shadow="none">
 				<CardHeader>
-					<div className="flex w-full justify-between gap-4 items-center">
+					<div className="flex flex-col sm:flex-row w-full justify-between gap-4 items-start sm:items-center">
 						<div className="flex flex-col gap-2 items-start">
 							<h4 className="text-xl font-medium">{data.title}</h4>
 
@@ -160,19 +160,21 @@ export default function JobDetails({
 							</p>
 						</div>
 
-						<Button size="lg">Apply now</Button>
+						<Button size="lg" className="w-full sm:w-auto">
+							Apply now
+						</Button>
 					</div>
 				</CardHeader>
-				<CardBody className="flex items-center justify-between gap-6 w-full">
-					<div className="flex justify-start items-center gap-8 w-full">
-						<p className="text-foreground-500 text-md font-normal">
+				<CardBody className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-6 w-full">
+					<div className="flex flex-col sm:flex-row justify-start items-start sm:items-center gap-8 w-full">
+						<p className="text-foreground-500 text-sm font-normal">
 							Posted on {formattedCreatedAt}
 						</p>
 						{salaryRange}
 					</div>
 				</CardBody>
-				<CardFooter className="flex justify-between w-full">
-					<div className="flex flex-wrap gap-8 items-center text-foreground-600 w-full">
+				<CardFooter className="flex flex-col sm:flex-row justify-between items-end sm:items-center w-full gap-6">
+					<div className="flex sm:flex-row flex-wrap gap-8 items-start sm:items-center text-foreground-600 w-full">
 						<p>{jobType(data.type)}</p>
 						<div className="flex items-center gap-2">
 							<MapPin size={16} />{" "}
