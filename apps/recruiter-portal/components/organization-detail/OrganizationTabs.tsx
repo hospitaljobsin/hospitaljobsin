8f5bebdb0dd43e420b39729e51e460e183a043b7
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-02-20 22:15:04 +0530

    add basic organization detail view

---
+++ apps/recruiter-portal/components/organization-detail/OrganizationTabs.tsx
@@ -0,0 +1,41 @@
+"use client";
+import links from "@/lib/links";
+import { Tab, Tabs } from "@heroui/react";
+import { HomeIcon, UserIcon } from "lucide-react";
+import { useParams, usePathname } from "next/navigation";
+
+export default function OrganizationTabs() {
+	const pathnname = usePathname();
+	const params = useParams<{ slug: string }>();
+	return (
+		<div className="flex w-full flex-col border-b border-gray-300 py-4">
+			<Tabs
+				aria-label="Organization Detail Menu"
+				color="default"
+				variant="light"
+				selectedKey={pathnname}
+			>
+				<Tab
+					key={links.organizationDetail(params.slug)}
+					href={links.organizationDetail(params.slug)}
+					title={
+						<div className="flex items-center space-x-2">
+							<HomeIcon />
+							<span>Overview</span>
+						</div>
+					}
+				/>
+				<Tab
+					key={links.organizationDetailMembers(params.slug)}
+					href={links.organizationDetailMembers(params.slug)}
+					title={
+						<div className="flex items-center space-x-2">
+							<UserIcon />
+							<span>Members</span>
+						</div>
+					}
+				/>
+			</Tabs>
+		</div>
+	);
+}
---
+++ apps/recruiter-portal/components/organization-detail/overview-tab/OrganizationOverviewTab.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable relay/must-colocate-fragment-spreads */
+"use client";
+import { graphql, useFragment } from "react-relay";
+import OrganizationDetails from "./OrganizationDetails";
+import type { OrganizationOverviewTabFragment$key } from "./__generated__/OrganizationOverviewTabFragment.graphql";
+
+const OrganizationOverviewTabFragment = graphql`
+ fragment OrganizationOverviewTabFragment on Query @argumentDefinitions(
+      slug: {
+        type: "String!",
+      }
+    ) {
+        ...OrganizationDetailsFragment @arguments(slug: $slug)
+  }
+`;
+
+export default function OrganizationOverviewTab(props: {
+	rootQuery: OrganizationOverviewTabFragment$key;
+}) {
+	const query = useFragment(OrganizationOverviewTabFragment, props.rootQuery);
+
+	return (
+		<div className="py-8 w-full h-full flex flex-col items-center gap-12">
+			<OrganizationDetails rootQuery={query} />
+		</div>
+	);
+}
