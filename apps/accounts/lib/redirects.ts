08755f19eab0cba86fe833f54bfe5b72382350c7
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-02-23 12:17:36 +0530

    simplify get valid redirect url function

--- apps/accounts/lib/redirects.ts
+++ apps/accounts/lib/redirects.ts
@@ -4,13 +4,9 @@ import links from "./links";
 // validate redirect to URLs to prevent open redirect attacks
 // https://thecopenhagenbook.com/open-redirect
 export function getValidRedirectURL(redirectTo: string | null): string {
-  if (redirectTo === null) {
-    return links.seekerLanding;
-  }
-
-  if (redirectTo.startsWith(env.NEXT_PUBLIC_RECRUITER_PORTAL_BASE_URL) || redirectTo.startsWith(env.NEXT_PUBLIC_SEEKER_PORTAL_BASE_URL)) {
-    return redirectTo;
-  }
-
-  return links.seekerLanding;
+	return redirectTo !== null &&
+		(redirectTo.startsWith(env.NEXT_PUBLIC_RECRUITER_PORTAL_BASE_URL) ||
+			redirectTo.startsWith(env.NEXT_PUBLIC_SEEKER_PORTAL_BASE_URL))
+		? redirectTo
+		: links.seekerLanding;
 }
