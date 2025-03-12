bae7e0f7e7bb91c38eb7c0845a34c063c1dd5c11
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-03-12 21:45:41 +0530

    install all playwright browsers

--- apps/accounts/app/auth/2fa/page.tsx
+++ apps/accounts/app/auth/2fa/page.tsx
@@ -1,18 +1,13 @@
+"use client";
+
 import TwoFactorAuthenticationForm from "@/components/auth/two-factor/TwoFactorAuthenticationForm";
 
 import type { Metadata } from "next";
-import { Suspense } from "react";
 
 export const metadata: Metadata = {
 	title: "Two Factor Authentication",
 };
 
 export default function TwoFactorAuthentication() {
-	// we need to wrap the form in a Suspense component
-	// because the form uses useSearchParams
-	return (
-		<Suspense>
-			<TwoFactorAuthenticationForm />
-		</Suspense>
-	);
+	return <TwoFactorAuthenticationForm />;
 }
