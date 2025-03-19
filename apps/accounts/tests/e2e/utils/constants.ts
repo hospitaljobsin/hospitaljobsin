b0e214234d6526b21910c7b2d380238fc5c0b30c
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-03-19 22:09:37 +0530

    fix e2e test constants typo

--- apps/accounts/tests/e2e/utils/constants.ts
+++ apps/accounts/tests/e2e/utils/constants.ts
@@ -2,7 +2,7 @@ export const TOTP_USER_SECRET = "RW5SJG5SRCHL3YEBPUOOIB6W5VDOF4MA";
 
 export const TESTER_EMAIL = "tester@example.org";
 
-export const TESTER_EMAIL_2 = "tester2@example.org"
+export const TESTER_EMAIL_2 = "tester2@example.org";
 
 export const NONEXISTENT_TESTER_EMAIL = "nonexistent-tester@example.org";
 
@@ -14,9 +14,8 @@ export const TWO_FACTOR_TESTER_2_EMAIL = "twofactor-tester2@example.org";
 
 export const RECOVERY_CODE_1 = "11111111";
 
-
 // cooldowns
 
-export const PASSWORD_RESET_TOKEN_COOLDONW = 20_000 + 2_000; // add buffer
+export const PASSWORD_RESET_TOKEN_COOLDOWN = 20_000 + 2_000; // add buffer
 
 export const EMAIL_VERIFICATION_TOKEN_COOLDOWN = 20_000 + 2_000; // add buffer
