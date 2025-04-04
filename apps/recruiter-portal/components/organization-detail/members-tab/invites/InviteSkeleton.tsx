058c0f480196c43d39f8ad29113b48ad8b4502ee
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-04-04 22:17:19 +0530

    update invite skeleton

--- apps/recruiter-portal/components/organization-detail/members-tab/invites/InviteSkeleton.tsx
+++ apps/recruiter-portal/components/organization-detail/members-tab/invites/InviteSkeleton.tsx
@@ -1,10 +1,19 @@
-import { Card, CardBody, Skeleton } from "@heroui/react";
+import { Card, CardBody, CardHeader, Skeleton } from "@heroui/react";
 
 export default function InviteSkeleton() {
 	return (
 		<Card fullWidth className="p-4 sm:p-6" isPressable={false} shadow="none">
-			<CardBody className="flex items-center gap-6 w-full flex-row">
+			<CardHeader className="w-full flex justify-between gap-6">
 				<Skeleton className="flex rounded-lg w-2/5 h-8" />
+				<Skeleton className="flex rounded-md w-8 h-8" />
+			</CardHeader>
+			<CardBody className="w-full flex gap-8 items-start sm:items-center flex-col sm:flex-row">
+				<Skeleton className="flex rounded-lg w-2/5 h-8" />
+				<Skeleton className="flex rounded-lg w-2/5 h-8" />
+				<div className="flex gap-4 w-full items-center">
+					<Skeleton className="flex rounded-lg w-2/5 h-8" />
+					<Skeleton className="flex rounded-full w-8 h-8" />
+				</div>
 			</CardBody>
 		</Card>
 	);
