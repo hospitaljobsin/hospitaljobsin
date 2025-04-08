7ddfcfa84c48a80b1be8e94ea3665c2f92e19fe2
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-04-08 22:06:42 +0530

    update recruiter portal job UI

--- apps/recruiter-portal/components/organization-detail/jobs-tab/Job.tsx
+++ apps/recruiter-portal/components/organization-detail/jobs-tab/Job.tsx
@@ -1,12 +1,9 @@
-import type { JobType } from "@/__generated__/JobDetailsInternalFragment.graphql";
-import type {
-	JobFragment$key,
-	WorkMode,
-} from "@/__generated__/JobFragment.graphql";
+import type { JobFragment$key } from "@/__generated__/JobFragment.graphql";
 import { getRelativeTimeString } from "@/lib/intl";
 import links from "@/lib/links";
 import { useRouter } from "@bprogress/next";
 import { Card, CardBody, CardFooter, CardHeader, Chip } from "@heroui/react";
+import { useParams } from "next/navigation";
 import { useFragment } from "react-relay";
 import { graphql } from "relay-runtime";
 
@@ -14,35 +11,9 @@ export const JobFragment = graphql`
   fragment JobFragment on Job {
     slug
     title
-    type
-    workMode
-    address {
-      city
-      state
-    }
     skills
-    currency
-    hasSalaryRange
-    minSalary
-    maxSalary
-    hasExperienceRange
-    minExperience
-    maxExperience
     createdAt
 	applicationCount
-    organization {
-      name
-      logoUrl
-	  slug
-      address {
-        city
-        state
-		line1
-		line2
-		pincode
-		country
-      }
-    }
   }
 `;
 
@@ -52,104 +23,34 @@ type Props = {
 
 export default function Job({ job }: Props) {
 	const router = useRouter();
+	const orgSlug = useParams<{ slug: string }>().slug;
 	const data = useFragment(JobFragment, job);
-
-	const jobType = (type: JobType) => {
-		switch (type) {
-			case "CONTRACT":
-				return "Contract";
-			case "FULL_TIME":
-				return "Full Time";
-			case "INTERNSHIP":
-				return "Internship";
-			case "PART_TIME":
-				return "Part Time";
-			default:
-				return null;
-		}
-	};
-
-	const workMode = (mode: WorkMode) => {
-		switch (mode) {
-			case "HYBRID":
-				return "Hybrid";
-			case "OFFICE":
-				return "Office";
-			case "REMOTE":
-				return "Remote";
-			default:
-				return null;
-		}
-	};
-
-	const formatAddress = (address: {
-		readonly city: string | null | undefined;
-		readonly state: string | null | undefined;
-		readonly country?: string | null | undefined;
-		readonly line1?: string | null | undefined;
-		readonly line2?: string | null | undefined;
-		readonly pincode?: string | null | undefined;
-	}) => {
-		const { city, state, country } = address;
-
-		// Build address parts with available components
-		const parts = [];
-		if (city) parts.push(city);
-		if (state) parts.push(state);
-		if (country) parts.push(country);
-
-		// Return combined parts or default message
-		return parts.length > 0 ? parts.join(", ") : "Not specified";
-	};
-
-	function formatExperienceRange({
-		hasExperienceRange,
-		minExperience,
-		maxExperience,
-	}: {
-		hasExperienceRange: boolean;
-		minExperience: number | null | undefined;
-		maxExperience: number | null | undefined;
-	}) {
-		if (hasExperienceRange) {
-			if (!minExperience) {
-				return `Maximum ${maxExperience} years`;
-			}
-			if (!maxExperience) {
-				return `Minimum ${minExperience} years`;
-			}
-			return `${minExperience} - ${maxExperience} years`;
-		}
-		return "Not specified";
-	}
 
 	return (
 		<Card
 			fullWidth
-			className="p-4 sm:p-6 cursor-pointer"
+			className="p-4 sm:p-6 cursor-pointer space-y-6"
 			isPressable
 			as="div"
 			disableRipple
 			onPress={() => {
-				router.push(
-					links.organizationJobDetail(data.organization.slug, data.slug),
-				);
+				router.push(links.organizationJobDetail(orgSlug, data.slug));
 			}}
 			shadow="none"
 		>
-			<CardHeader>
+			<CardHeader className="flex flex-col gap-6 w-full items-start">
 				<h4 className="text-lg/7 sm:text-xl/8 font-medium text-balance">
 					{data.title}
 				</h4>
-			</CardHeader>
-			<CardBody className="flex flex-col gap-10 w-full">
-				<div className="flex flex-wrap gap-2 sm:gap-4 mt-2 w-full justify-start">
+				<div className="flex flex-wrap gap-2 sm:gap-4 w-full justify-start">
 					{data.skills.map((skill) => (
 						<Chip variant="flat" key={skill}>
 							{skill}
 						</Chip>
 					))}
 				</div>
+			</CardHeader>
+			<CardBody className="flex flex-col gap-10 w-full">
 				<div className="w-full flex flex-wrap justify-between items-center gap-4 sm:gap-8">
 					<div className="flex flex-col items-center gap-2">
 						<h2 className="text-2xl font-medium text-foreground-700">
