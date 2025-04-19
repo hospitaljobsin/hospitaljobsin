bb4d3e0392c19b7ebccaaf3e60db0462e5b4b8b3
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-04-19 08:56:22 +0530

    add infra TODO notes

--- infrastructure/github.tf
+++ infrastructure/github.tf
@@ -1,5 +1,8 @@
 # TODO: create an IAM user/ role with ECR permissions alone (least privilege)
 # then store the AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in github actions secrets here
+# also set AWS_REGION in github actions variables
 # also store the backend function name as a secret/ variable in github actions here
 
 # using the Hashicorp Github provider
+
+# TODO: also set backend image name here
