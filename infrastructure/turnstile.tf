cc3ffe52ad1a1fbf405362413b94b3534598cb07
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-04-22 18:29:52 +0530

    add localhost as turnstile domain

--- infrastructure/turnstile.tf
+++ infrastructure/turnstile.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_turnstile_widget" "example" {
   account_id = var.cloudflare_acount_id
   name       = "My Terraform-managed widget"
-  domains    = ["${var.domain_name}", "recruiter.${var.domain_name}", "accounts.${var.domain_name}"]
+  domains    = ["${var.domain_name}", "recruiter.${var.domain_name}", "accounts.${var.domain_name}", "localhost"]
   mode       = "managed"
 }
