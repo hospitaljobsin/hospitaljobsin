ac1109bf3ae8c8c40f07ffad63d41360ac48d289
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-04-22 12:05:24 +0530

    update jwe secret key generation

--- infrastructure/secrets.tf
+++ infrastructure/secrets.tf
@@ -1,3 +1,7 @@
+resource "random_bytes" "jwt_secret" {
+  length = 64
+}
+
 resource "aws_secretsmanager_secret" "backend" {
   name        = "${var.resource_prefix}/backend"
   description = "Production settings secret for the backend"
@@ -6,7 +10,7 @@ resource "aws_secretsmanager_secret" "backend" {
 resource "aws_secretsmanager_secret_version" "example" {
   secret_id = aws_secretsmanager_secret.backend.id
   secret_string = jsonencode({
-    server_jwe_secret_key       = "test",
+    server_jwe_secret_key       = random_bytes.jwt_secret.base64,
     server_google_client_id     = "test",
     server_google_client_secret = "test",
     server_recaptcha_secret_key = "test"
