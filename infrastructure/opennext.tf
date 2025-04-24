01ffe933c970e7ff12ee3bd819f70487ab5f86f0
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-04-24 08:59:59 +0530

    update opennext modules' regions

--- infrastructure/opennext.tf
+++ infrastructure/opennext.tf
@@ -5,6 +5,7 @@ module "opennext_seeker_portal" {
   prefix              = "${var.resource_prefix}-seekerportal"
   opennext_build_path = "../apps/seeker-portal/.open-next" # Path to your .open-next folder
   hosted_zone_id      = aws_route53_zone.main.zone_id      # The Route53 hosted zone ID for your domain name
+  region              = var.aws_region
 
   cloudfront = {
     aliases             = [aws_acm_certificate.seeker_portal_cert.domain_name]              # Your domain name
@@ -20,6 +21,7 @@ module "opennext_recruiter_portal" {
   prefix              = "${var.resource_prefix}-recruiterportal"
   opennext_build_path = "../apps/recruiter-portal/.open-next" # Path to your .open-next folder
   hosted_zone_id      = aws_route53_zone.main.zone_id         # The Route53 hosted zone ID for your domain name
+  region              = var.aws_region
 
   cloudfront = {
     aliases             = [aws_acm_certificate.recruiter_portal_cert.domain_name]              # Your domain name
@@ -35,6 +37,7 @@ module "opennext_accounts" {
   prefix              = "${var.resource_prefix}-accounts"
   opennext_build_path = "../apps/accounts/.open-next" # Path to your .open-next folder
   hosted_zone_id      = aws_route53_zone.main.zone_id # The Route53 hosted zone ID for your domain name
+  region              = var.aws_region
 
   cloudfront = {
     aliases             = [aws_acm_certificate.accounts_cert.domain_name]              # Your domain name
