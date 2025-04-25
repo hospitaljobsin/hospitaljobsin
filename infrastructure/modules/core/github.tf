3085ea582a31555edf77ba7073365f74ef6bfdf0
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-04-25 18:34:10 +0530

    restart infra deployments

--- infrastructure/modules/core/github.tf
+++ infrastructure/modules/core/github.tf
@@ -16,6 +16,30 @@ resource "github_actions_secret" "aws_secret_access_key" {
   plaintext_value = aws_iam_access_key.github_actions.secret
 }
 
+resource "github_actions_secret" "sst_jwe_secret_key" {
+  repository      = data.github_repository.this.name
+  secret_name     = "SST_JWE_SECRET_KEY"
+  plaintext_value = random_bytes.jwt_secret.base64
+}
+
+resource "github_actions_variable" "sst_api_url" {
+  repository    = data.github_repository.this.name
+  variable_name = "SST_API_URL"
+  value         = aws_lambda_function.backend.function_name
+}
+
+resource "github_actions_variable" "sst_vpc_id" {
+  repository    = data.github_repository.this.name
+  variable_name = "SST_VPC_ID"
+  value         = aws_vpc.this.id
+}
+
+resource "github_actions_variable" "sst_accounts_domain" {
+  repository    = data.github_repository.this.name
+  variable_name = "SST_ACCOUNTS_DOMAIN"
+  value         = "accounts.${var.domain_name}"
+}
+
 # Store the backend function name as a variable in GitHub Actions
 resource "github_actions_variable" "aws_lambda_backend_function_name" {
   repository    = data.github_repository.this.name
