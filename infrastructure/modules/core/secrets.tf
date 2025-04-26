21b44b6d89eddc350d24d59724428454761684ed
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-04-26 17:26:26 +0530

    update jwt secret gen

--- infrastructure/modules/core/secrets.tf
+++ infrastructure/modules/core/secrets.tf
@@ -1,4 +1,4 @@
-resource "random_bytes" "jwt_secret" {
+resource "random_bytes" "jwe_secret" {
   length = 32
 }
 
@@ -10,9 +10,11 @@ resource "aws_secretsmanager_secret" "backend" {
 resource "aws_secretsmanager_secret_version" "example" {
   secret_id = aws_secretsmanager_secret.backend.id
   secret_string = jsonencode({
-    server_jwe_secret_key       = random_bytes.jwt_secret.base64,
+    server_jwe_secret_key       = random_bytes.jwe_secret.base64,
     server_google_client_id     = var.google_oauth_client_id,
     server_google_client_secret = var.google_oauth_client_secret,
     server_captcha_secret_key   = cloudflare_turnstile_widget.example.secret
   })
+
+  depends_on = [random_bytes.jwe_secret, cloudflare_turnstile_widget.example]
 }
