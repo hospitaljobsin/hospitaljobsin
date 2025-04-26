1253c0f5a00ed103872e8f45d3802d1cc2ef14c4
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-04-26 20:12:42 +0530

    load sensitive values via secrets in frontend server handlers

--- apps/accounts/sst.config.ts
+++ apps/accounts/sst.config.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
-import { env } from "./lib/env";
+const { env } = await import("./lib/env");
 
 export default $config({
 	app(input) {
