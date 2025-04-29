fcaf77bcc4036437de991f194f973e0482741fd9
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-04-29 19:26:42 +0530

    update sessions settings skeleton

--- apps/accounts/components/settings/sessions/SessionsListSkeleton.tsx
+++ apps/accounts/components/settings/sessions/SessionsListSkeleton.tsx
@@ -3,16 +3,11 @@ import SessionsControllerSkeleton from "./SessionsControllerSkeleton";
 
 export default function SessionsListSkeleton() {
 	return (
-		<div className="w-full flex flex-col gap-6">
-			<div className="flex w-full items-center justify-between gap-6">
-				<p className="text-foreground-600">My Sessions</p>
-				<SessionsControllerSkeleton />
-			</div>
+
 			<div className="w-full h-full flex flex-col gap-4 sm:gap-8 pb-4 sm:pb-6">
 				{Array.from({ length: 3 }).map((_, index) => (
 					<SessionSkeleton key={`Session-skeleton-${index}`} />
 				))}
 			</div>
-		</div>
 	);
 }
--- apps/accounts/components/settings/sessions/SessionsSettingsViewSkeleton.tsx
+++ apps/accounts/components/settings/sessions/SessionsSettingsViewSkeleton.tsx
@@ -4,7 +4,12 @@ import SessionsListSkeleton from "./SessionsListSkeleton";
 export default function SessionsSettingsViewSkeleton() {
 	return (
 		<div className="w-full h-full space-y-6">
-			<SessionsListSkeleton />
+					<div className="w-full flex flex-col gap-6">
+						<div className="flex w-full items-center justify-between gap-6">
+							<p className="text-foreground-600">My Sessions</p>
+							<SessionsControllerSkeleton />
+						</div>
+			<SessionsListSkeleton /></div>
 		</div>
 	);
 }
