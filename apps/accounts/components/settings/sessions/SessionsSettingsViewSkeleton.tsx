cda603396ca4751d450825d651e163ea1d8aa38e
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-04-29 19:26:49 +0530

    update sessions settings skeleton

--- apps/accounts/components/settings/sessions/SessionsSettingsViewSkeleton.tsx
+++ apps/accounts/components/settings/sessions/SessionsSettingsViewSkeleton.tsx
@@ -1,15 +1,17 @@
 "use client";
+import SessionsControllerSkeleton from "./SessionsControllerSkeleton";
 import SessionsListSkeleton from "./SessionsListSkeleton";
 
 export default function SessionsSettingsViewSkeleton() {
 	return (
 		<div className="w-full h-full space-y-6">
-					<div className="w-full flex flex-col gap-6">
-						<div className="flex w-full items-center justify-between gap-6">
-							<p className="text-foreground-600">My Sessions</p>
-							<SessionsControllerSkeleton />
-						</div>
-			<SessionsListSkeleton /></div>
+			<div className="w-full flex flex-col gap-6">
+				<div className="flex w-full items-center justify-between gap-6">
+					<p className="text-foreground-600">My Sessions</p>
+					<SessionsControllerSkeleton />
+				</div>
+				<SessionsListSkeleton />
+			</div>
 		</div>
 	);
 }
