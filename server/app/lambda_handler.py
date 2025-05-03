1f8df7ae62a8e005ed16d6e6154a13e22852d07d
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-05-03 16:56:17 +0530

    add dummy lambda handler for now

--- server/app/lambda_handler.py
+++ server/app/lambda_handler.py
@@ -1,36 +1,36 @@
-# from mangum import Mangum
-
-# from app import create_app
-
-# app = create_app()
-# handler = Mangum(app, lifespan="auto")
-
-from collections.abc import AsyncGenerator
-from contextlib import asynccontextmanager
-
-from fastapi import FastAPI
 from mangum import Mangum
 
+from app import create_app
 
-@asynccontextmanager
-async def app_lifespan(_app: FastAPI) -> AsyncGenerator[None, None]:
-    """Initialize the database when the app starts."""
-    print("before app start")
-    yield
-    print("after app stop")
+app = create_app()
+handler = Mangum(app, lifespan="auto")
 
+# from collections.abc import AsyncGenerator
+# from contextlib import asynccontextmanager
 
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# from fastapi import FastAPI
+# from mangum import Mangum
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+# @asynccontextmanager
+# async def app_lifespan(_app: FastAPI) -> AsyncGenerator[None, None]:
+#     """Initialize the database when the app starts."""
+#     print("before app start")
+#     yield
+#     print("after app stop")
 
 
-handler = Mangum(app, lifespan="on")
+# app = FastAPI()
+
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str = None):
+#     return {"item_id": item_id, "q": q}
+
+
+# handler = Mangum(app, lifespan="on")
