ae1e4983c2d8416588e99319f6b68bbf37be472d
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-05-03 12:44:50 +0530

    add dummy lambda handler for now

--- server/app/lambda_handler.py
+++ server/app/lambda_handler.py
@@ -1,6 +1,23 @@
+# from mangum import Mangum
+
+# from app import create_app
+
+# app = create_app()
+# handler = Mangum(app, lifespan="auto")
+
+
 from mangum import Mangum
 
-from app import create_app
 
-app = create_app()
-handler = Mangum(app, lifespan="auto")
+async def app(scope, receive, send):
+    await send(
+        {
+            "type": "http.response.start",
+            "status": 200,
+            "headers": [[b"content-type", b"text/plain; charset=utf-8"]],
+        }
+    )
+    await send({"type": "http.response.body", "body": b"Hello, world!"})
+
+
+handler = Mangum(app, lifespan="off")
