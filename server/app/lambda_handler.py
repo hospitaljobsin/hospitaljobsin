384520ffa247564f4cd689cc51e704a5ee2680e2
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-05-03 16:43:51 +0530

    add dummy lambda handler for now

--- server/app/lambda_handler.py
+++ server/app/lambda_handler.py
@@ -5,8 +5,20 @@
 # app = create_app()
 # handler = Mangum(app, lifespan="auto")
 
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from mangum import Mangum
+
+
+@asynccontextmanager
+async def app_lifespan(_app: FastAPI) -> AsyncGenerator[None, None]:
+    """Initialize the database when the app starts."""
+    print("before app start")
+    yield
+    print("after app stop")
+
 
 app = FastAPI()
 
@@ -21,4 +33,4 @@ def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 
-handler = Mangum(app, lifespan="off")
+handler = Mangum(app, lifespan="on")
