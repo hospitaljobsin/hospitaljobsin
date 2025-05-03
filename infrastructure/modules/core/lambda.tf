c12c1632e92e44607f66e3735139d4b502b37538
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-05-03 11:40:19 +0530

    add parameters and secrets extension lambda layer to backend lambda

--- infrastructure/modules/core/lambda.tf
+++ infrastructure/modules/core/lambda.tf
@@ -149,6 +149,9 @@ resource "aws_lambda_function" "backend" {
   depends_on    = [docker_registry_image.backend]
   function_name = "${var.resource_prefix}-backend-lambda"
 
+  timeout = 10
+
+
   role         = aws_iam_role.lambda_exec_role.arn
   package_type = "Image"
   image_uri    = "${aws_ecr_repository.backend.repository_url}:latest"
