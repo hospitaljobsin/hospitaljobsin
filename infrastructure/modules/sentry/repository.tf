9867f6240ac8ff55548b70437f6143de177ae1ce
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-05-12 19:43:23 +0530

    add sentry org code mapping

--- infrastructure/modules/sentry/repository.tf
+++ infrastructure/modules/sentry/repository.tf
@@ -7,6 +7,6 @@ data "sentry_organization_integration" "github" {
 resource "sentry_organization_repository" "github" {
   organization     = data.sentry_organization.main.id
   integration_type = "github"
-  integration_id   = data.sentry_organization_integration.github.internal_id
+  integration_id   = data.sentry_organization_integration.github.id
   identifier       = var.github_repository_name
 }
