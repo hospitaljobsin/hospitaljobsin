6773a248db4027de6b01030adc1b20c2a5a99579
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-05-12 19:52:33 +0530

    add org code mappings for all projects

--- infrastructure/modules/sentry/projects.tf
+++ infrastructure/modules/sentry/projects.tf
@@ -35,6 +35,17 @@ resource "sentry_project_spike_protection" "accounts_ui" {
   enabled      = true
 }
 
+resource "sentry_organization_code_mapping" "accounts_ui" {
+  organization   = data.sentry_organization.main.id
+  integration_id = data.sentry_organization_integration.github.id
+  repository_id  = sentry_organization_repository.github.id
+  project_id     = sentry_project.accounts_ui.internal_id
+
+  default_branch = "main"
+  stack_root     = "/"
+  source_root    = "apps/accounts/"
+}
+
 resource "sentry_project" "seeker_portal_ui" {
   organization = sentry_team.main.organization
   teams        = [sentry_team.main.id]
@@ -48,6 +59,16 @@ resource "sentry_project_spike_protection" "seeker_portal_ui" {
   enabled      = true
 }
 
+resource "sentry_organization_code_mapping" "seeker_portal_ui" {
+  organization   = data.sentry_organization.main.id
+  integration_id = data.sentry_organization_integration.github.id
+  repository_id  = sentry_organization_repository.github.id
+  project_id     = sentry_project.seeker_portal_ui.internal_id
+
+  default_branch = "main"
+  stack_root     = "/"
+  source_root    = "apps/seeker-portal/"
+}
 
 resource "sentry_project" "recruiter_portal_ui" {
   organization = sentry_team.main.organization
@@ -60,4 +81,15 @@ resource "sentry_project_spike_protection" "recruiter_portal_ui" {
   organization = sentry_project.recruiter_portal_ui.organization
   project      = sentry_project.recruiter_portal_ui.id
   enabled      = true
+}
+
+resource "sentry_organization_code_mapping" "recruiter_portal_ui" {
+  organization   = data.sentry_organization.main.id
+  integration_id = data.sentry_organization_integration.github.id
+  repository_id  = sentry_organization_repository.github.id
+  project_id     = sentry_project.recruiter_portal_ui.internal_id
+
+  default_branch = "main"
+  stack_root     = "/"
+  source_root    = "apps/recruiter-portal/"
 }
