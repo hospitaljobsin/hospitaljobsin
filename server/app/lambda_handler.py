a6743e657f9cd57fa4d4537d2cde0c414add8b0d
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-05-18 07:48:31 +0530

    setup logging before initializing Mangum handler

--- server/app/lambda_handler.py
+++ server/app/lambda_handler.py
@@ -3,8 +3,17 @@ from mangum import Mangum
 from app import create_app
 from app.config import AppSettings, get_settings
 from app.core.instrumentation import initialize_instrumentation
+from app.logger import setup_logging
 
-initialize_instrumentation(settings=get_settings(AppSettings))
+settings = get_settings(AppSettings)
+
+# setup instrumentation
+initialize_instrumentation(settings=settings)
+
+# set up logging
+setup_logging(
+    human_readable=settings.debug,
+)
 
 app = create_app()
 handler = Mangum(app, lifespan="auto")
