751e8ee29f82e67a848aa5f614ec03ab65167f0b
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-05-25 09:36:49 +0000

    isolate tests: enable/disable 2fa within the same test, with distinct steps

--- e2e/tests/accounts/settings/account.spec.ts
+++ e2e/tests/accounts/settings/account.spec.ts
@@ -5,129 +5,6 @@ import { Jimp } from "jimp";
 import fs from "node:fs";
 import QrCode from "qrcode-reader";
 
-authTest.describe("Account Settings Page- 2FA", () => {
-	authTest.beforeEach(async ({ page }) => {
-		// Navigate to login page
-		await page.goto("http://localhost:5002/settings");
-	});
-
-	authTest(
-		"should allow user to enable 2FA authenticator",
-		async ({ page, browserName }) => {
-			await expect(page.getByRole("button", { name: /enable/i })).toBeVisible();
-
-			await Promise.all([
-				page.getByRole("button", { name: /enable/i }).click(),
-				page.waitForSelector('section[role="dialog"][data-open="true"]', {
-					state: "visible",
-				}),
-			]);
-
-			await expect(
-				page.getByText("Enable Two Factor Authentication"),
-			).toBeVisible();
-
-			// we see a QR code and TOTP secret here
-			const qrLocator = page.locator('[data-testid="totp-qrcode"]'); // Adjust selector if needed
-			await qrLocator.screenshot({
-				path: "playwright/screenshots/2fa-qrcode.png",
-			});
-
-			// Decode the QR code from the screenshot
-			const image = await Jimp.read(
-				fs.readFileSync("playwright/screenshots/2fa-qrcode.png"),
-			);
-
-			const qr = new QrCode();
-			let totpSecretUrl = "";
-
-			qr.callback = (
-				err: ErrorOptions | undefined,
-				value: { result: string },
-			) => {
-				if (err) {
-					throw new Error("❌ Failed to decode QR code:", err);
-				}
-				totpSecretUrl = value.result;
-				console.log("✅ QR code content:", value.result);
-			};
-			qr.decode(image.bitmap);
-
-			const totpSecret = totpSecretUrl.split("secret=")[1].split("&")[0];
-
-			if (browserName !== "webkit") {
-				// we have navigation API permissions issues on webkit, so we cannot copy to clipboard
-				// https://github.com/microsoft/playwright/issues/13037
-				await page
-					.getByRole("button", { name: /copy secret to clipboard/i })
-					.click();
-
-				const copiedTotpSecret = await page.evaluate(() =>
-					navigator.clipboard.readText(),
-				);
-
-				expect(totpSecret).toEqual(copiedTotpSecret);
-			}
-
-			const otp = await generateValidOTP({ totp_secret: totpSecret });
-
-			await page.locator('input[name="token"]').fill(otp);
-
-			await page.getByRole("button", { name: /enable/i }).click();
-
-			// save recovery codes
-			await Promise.all([
-				// Wait for the modal to close
-				page.waitForSelector('section[role="dialog"][data-open="true"]', {
-					state: "detached",
-				}),
-				// Click the enable button
-				page
-					.getByRole("button", { name: /I've saved my recovery codes/i })
-					.click(),
-			]);
-
-			await expect(
-				page.getByRole("button", { name: /disable/i }),
-			).toBeVisible();
-		},
-	);
-
-	authTest(
-		"should allow user to disable 2FA authenticator",
-		async ({ page }) => {
-			await expect(
-				page.getByRole("button", { name: /disable/i }),
-			).toBeVisible();
-
-			await Promise.all([
-				page.getByRole("button", { name: /disable/i }).click(),
-				page.waitForSelector('section[role="dialog"][data-open="true"]', {
-					state: "visible",
-				}),
-			]);
-
-			await expect(
-				page.getByRole("heading", {
-					name: "Disable Two Factor Authentication",
-				}),
-			).toBeVisible();
-
-			// disable 2FA
-			await Promise.all([
-				// Wait for the modal to close
-				page.waitForSelector('section[role="dialog"][data-open="true"]', {
-					state: "detached",
-				}),
-				// Click the enable button
-				page
-					.getByRole("button", { name: /disable/i })
-					.click(),
-			]);
-		},
-	);
-});
-
 authTest.describe("Account Settings Page", () => {
 	authTest.beforeEach(async ({ page }) => {
 		// Navigate to login page
@@ -168,6 +45,123 @@ authTest.describe("Account Settings Page", () => {
 		// Wait for the form to close and the new name to appear
 		await expect(page.getByText(newName)).toBeVisible();
 	});
+
+	authTest(
+		"should allow user to enable/disable 2FA authenticator",
+		async ({ page, browserName }) => {
+			await authTest.step("Enable 2FA Authenticator", async () => {
+				await expect(
+					page.getByRole("button", { name: /enable/i }),
+				).toBeVisible();
+
+				await Promise.all([
+					page.getByRole("button", { name: /enable/i }).click(),
+					page.waitForSelector('section[role="dialog"][data-open="true"]', {
+						state: "visible",
+					}),
+				]);
+
+				await expect(
+					page.getByText("Enable Two Factor Authentication"),
+				).toBeVisible();
+
+				// we see a QR code and TOTP secret here
+				const qrLocator = page.locator('[data-testid="totp-qrcode"]'); // Adjust selector if needed
+				await qrLocator.screenshot({
+					path: "playwright/screenshots/2fa-qrcode.png",
+				});
+
+				// Decode the QR code from the screenshot
+				const image = await Jimp.read(
+					fs.readFileSync("playwright/screenshots/2fa-qrcode.png"),
+				);
+
+				const qr = new QrCode();
+				let totpSecretUrl = "";
+
+				qr.callback = (
+					err: ErrorOptions | undefined,
+					value: { result: string },
+				) => {
+					if (err) {
+						throw new Error("❌ Failed to decode QR code:", err);
+					}
+					totpSecretUrl = value.result;
+					console.log("✅ QR code content:", value.result);
+				};
+				qr.decode(image.bitmap);
+
+				const totpSecret = totpSecretUrl.split("secret=")[1].split("&")[0];
+
+				if (browserName !== "webkit") {
+					// we have navigation API permissions issues on webkit, so we cannot copy to clipboard
+					// https://github.com/microsoft/playwright/issues/13037
+					await page
+						.getByRole("button", { name: /copy secret to clipboard/i })
+						.click();
+
+					const copiedTotpSecret = await page.evaluate(() =>
+						navigator.clipboard.readText(),
+					);
+
+					expect(totpSecret).toEqual(copiedTotpSecret);
+				}
+
+				const otp = await generateValidOTP({ totp_secret: totpSecret });
+
+				await page.locator('input[name="token"]').fill(otp);
+
+				await page.getByRole("button", { name: /enable/i }).click();
+
+				// save recovery codes
+				await Promise.all([
+					// Wait for the modal to close
+					page.waitForSelector('section[role="dialog"][data-open="true"]', {
+						state: "detached",
+					}),
+					// Click the enable button
+					page
+						.getByRole("button", { name: /I've saved my recovery codes/i })
+						.click(),
+				]);
+
+				await expect(
+					page.getByRole("button", { name: /disable/i }),
+				).toBeVisible();
+			});
+
+			await authTest.step("Disable 2FA Authenticator", async () => {
+				await expect(
+					page.getByRole("button", { name: /disable/i }),
+				).toBeVisible();
+
+				await Promise.all([
+					page.getByRole("button", { name: /disable/i }).click(),
+					page.waitForSelector('section[role="dialog"][data-open="true"]', {
+						state: "visible",
+					}),
+				]);
+
+				await expect(
+					page.getByRole("heading", {
+						name: "Disable Two Factor Authentication",
+					}),
+				).toBeVisible();
+
+				// disable 2FA
+				await Promise.all([
+					// Wait for the modal to close
+					page.waitForSelector('section[role="dialog"][data-open="true"]', {
+						state: "detached",
+					}),
+					// Click the enable button
+					page
+						.getByRole("button", { name: /disable/i })
+						.click(),
+				]);
+			});
+		},
+	);
 
 	authTest("should disable delete password button", async ({ page }) => {
 		// delete password button should be disabled when the password is the only auth provider
