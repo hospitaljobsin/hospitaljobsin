2ef26617d0310b2ac4f73069f629bd16e2ffd2bc
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-05-28 06:47:17 +0000

    use fetch policy: store-or-network for settings HeaderClientComponent

--- apps/accounts/app/settings/HeaderClientComponent.tsx
+++ apps/accounts/app/settings/HeaderClientComponent.tsx
@@ -1,16 +1,21 @@
 "use client";
-import { Suspense } from "react";
-import { loadQuery, useRelayEnvironment } from "react-relay";
 import type { HeaderQuery as HeaderQueryType } from "@/__generated__/HeaderQuery.graphql";
 import Header, { HeaderQuery } from "@/components/settings/Header";
 import HeaderSkeleton from "@/components/settings/HeaderSkeleton";
+import { Suspense } from "react";
+import { loadQuery, useRelayEnvironment } from "react-relay";
 
 export default function HeaderClientComponent() {
 	const environment = useRelayEnvironment();
 	const queryReference = loadQuery<HeaderQueryType>(
 		environment,
 		HeaderQuery,
-		{},
+		{
+			// You can pass any variables needed for the query here
+		},
+		{
+			fetchPolicy: "store-or-network",
+		},
 	);
 	return (
 		<Suspense fallback={<HeaderSkeleton />}>
