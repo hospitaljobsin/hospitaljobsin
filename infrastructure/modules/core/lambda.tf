ada3540ea98425c329508e26ae79cee46b22443a
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-05-31 16:33:23 +0000

    update env vars

--- infrastructure/modules/core/lambda.tf
+++ infrastructure/modules/core/lambda.tf
@@ -184,14 +184,14 @@ resource "aws_lambda_function" "backend" {
 
   environment {
     variables = {
-      SERVER_DEBUG                                = "false"
-      SERVER_ENVIRONMENT                          = "production"
-      SERVER_DATABASE_URL                         = "${mongodbatlas_advanced_cluster.this.connection_strings[0].standard_srv}?authMechanism=MONGODB-AWS&authSource=$external"
-      SERVER_DEFAULT_DATABASE_NAME                = var.mongodb_database_name
-      SERVER_HOST                                 = "0.0.0.0"
-      SERVER_PORT                                 = "8000"
-      SERVER_LOG_LEVEL                            = "INFO"
-      SERVER_CORS_ALLOW_ORIGINS                   = "[\"https://${var.domain_name}\", \"https://recruiter.${var.domain_name}\", \"https://accounts.${var.domain_name}\"]"
+      SERVER_DEBUG                 = "false"
+      SERVER_ENVIRONMENT           = "production"
+      SERVER_DATABASE_URL          = "${mongodbatlas_advanced_cluster.this.connection_strings[0].standard_srv}?authMechanism=MONGODB-AWS&authSource=$external"
+      SERVER_DEFAULT_DATABASE_NAME = var.mongodb_database_name
+      SERVER_HOST                  = "0.0.0.0"
+      SERVER_PORT                  = "8000"
+      SERVER_LOG_LEVEL             = "INFO"
+      # SERVER_CORS_ALLOW_ORIGINS                   = "[\"https://${var.domain_name}\", \"https://recruiter.${var.domain_name}\", \"https://accounts.${var.domain_name}\"]"
       SERVER_CORS_ALLOW_ORIGIN_REGEX              = "https://.*\\.${local.escaped_domain}"
       SERVER_SESSION_COOKIE_DOMAIN                = var.domain_name
       SERVER_EMAIl_PROVIDER                       = "aws_ses"
