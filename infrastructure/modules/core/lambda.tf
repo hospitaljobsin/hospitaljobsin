2ed8eeeb19b5c5a7b89eabd87e2be14231ef916a
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-05-31 15:53:34 +0000

    update server cors allow origin regex

--- infrastructure/modules/core/lambda.tf
+++ infrastructure/modules/core/lambda.tf
@@ -192,7 +192,7 @@ resource "aws_lambda_function" "backend" {
       SERVER_PORT                                 = "8000"
       SERVER_LOG_LEVEL                            = "INFO"
       SERVER_CORS_ALLOW_ORIGINS                   = "[\"https://${var.domain_name}\", \"https://recruiter.${var.domain_name}\", \"https://accounts.${var.domain_name}\"]"
-      SERVER_CORS_ALLOW_ORIGIN_REGEX              = "^https://.*\\.${local.escaped_domain}$"
+      SERVER_CORS_ALLOW_ORIGIN_REGEX              = "https://.*\\.${local.escaped_domain}"
       SERVER_SESSION_COOKIE_DOMAIN                = var.domain_name
       SERVER_EMAIl_PROVIDER                       = "aws_ses"
       SERVER_EMAIL_FROM                           = aws_ses_email_identity.this.email
