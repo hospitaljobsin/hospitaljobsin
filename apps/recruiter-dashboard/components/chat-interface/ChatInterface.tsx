6a34a1a192a0fb2d251e4eaf5951a2735def6c15
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-06-09 15:54:42 +0000

    update message viewer switch button

--- apps/recruiter-dashboard/components/chat-interface/ChatInterface.tsx
+++ apps/recruiter-dashboard/components/chat-interface/ChatInterface.tsx
@@ -4,12 +4,7 @@ import { useCopilotChat } from "@copilotkit/react-core";
 import { Role, TextMessage } from "@copilotkit/runtime-client-gql";
 import { Button, Input } from "@heroui/react";
 import { standardSchemaResolver } from "@hookform/resolvers/standard-schema";
-import {
-	ChevronLeft,
-	ChevronRight,
-	Sparkles,
-	StopCircleIcon,
-} from "lucide-react";
+import { ChevronDown, ChevronUp, StopCircleIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod/v4-mini";
 import { useMessageViewer } from "../MessageViewerProvider";
@@ -49,43 +44,29 @@ export default function ChatInterface({
 
 	return (
 		<div className="w-full mx-auto bottom-0 shrink sticky">
-			<div className="w-full relative">
-				<div className="absolute top-0 left-0 right-0 -mt-6 mx-auto max-w-5xl flex justify-start">
+			<div className="w-full bg-background-400 p-6 px-5 border-t border-gray-300">
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="mx-auto max-w-5xl flex items-center gap-4"
+				>
 					<Button
-						variant="shadow"
-						hidden={isEmpty}
-						isDisabled={isLoading}
-						startContent={
-							showMessageViewer ? (
-								<ChevronLeft size={20} className="text-foreground-400" />
-							) : (
-								<ChevronRight size={20} className="text-foreground-400" />
-							)
-						}
+						title={showMessageViewer ? "back to dashboard" : "view messages"}
+						variant="faded"
+						isDisabled={isLoading || isEmpty}
+						isIconOnly
 						onPress={() => {
 							setShowMessageViewer((status) => !status);
 						}}
 					>
 						{showMessageViewer ? (
-							<p>back to dashboard</p>
+							<ChevronDown size={20} className="text-foreground-400" />
 						) : (
-							<p>view messages</p>
+							<ChevronUp size={20} className="text-foreground-400" />
 						)}
 					</Button>
-				</div>
-			</div>
-
-			<div className="w-full bg-background-400 p-8 px-5 border-t border-gray-300">
-				<form
-					onSubmit={form.handleSubmit(onSubmit)}
-					className="mx-auto max-w-5xl"
-				>
 					<Input
 						fullWidth
 						variant="faded"
-						startContent={
-							<Sparkles size={20} className="text-foreground-400 mx-4" />
-						}
 						placeholder={placeholder}
 						size="lg"
 						{...form.register("message")}
