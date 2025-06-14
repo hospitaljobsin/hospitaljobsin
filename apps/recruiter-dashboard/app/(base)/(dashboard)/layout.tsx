20a73c1c3bcb8765a6eb5c66e8278e3539bcd7e7
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-06-14 16:44:13 +0000

    fix scrolling issues in dashboard page

--- apps/recruiter-dashboard/app/(base)/(dashboard)/layout.tsx
+++ apps/recruiter-dashboard/app/(base)/(dashboard)/layout.tsx
@@ -7,11 +7,9 @@ export default function DashboardLayout({
 }) {
 	return (
 		<div className="flex flex-col h-full w-full">
-			<div className="w-full mx-auto grow h-full overflow-hidden">
+			<div className="w-full mx-auto grow h-full">
 				<div className="w-full h-full relative">
-					<div className="w-full max-w-7xl h-full overflow-y-auto">
-						{children}
-					</div>
+					<div className="w-full max-w-7xl h-full">{children}</div>
 				</div>
 			</div>
 		</div>
