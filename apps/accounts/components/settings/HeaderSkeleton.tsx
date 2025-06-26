8a104b4e1acc1b991dc8d244e548a6af01087dd7
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-06-26 09:44:36 +0000

    make accounts header skeleton consistent

--- apps/accounts/components/settings/HeaderSkeleton.tsx
+++ apps/accounts/components/settings/HeaderSkeleton.tsx
@@ -3,12 +3,17 @@ import { APP_NAME } from "@/lib/constants";
 import links from "@/lib/links";
 import { Navbar, NavbarBrand } from "@heroui/react";
 import Link from "next/link";
+import Logo from "../Logo";
 
 export default function HeaderSkeleton() {
 	return (
 		<Navbar maxWidth="lg" isBordered className="bg-background-600">
-			<NavbarBrand>
-				<Link href={links.settings} className="font-medium text-inherit">
+			<NavbarBrand className="flex items-center gap-4 text-foreground-500">
+				<Link
+					href={links.settings}
+					className="font-medium text-inherit flex items-center gap-4"
+				>
+					<Logo />
 					{APP_NAME}
 				</Link>
 			</NavbarBrand>
