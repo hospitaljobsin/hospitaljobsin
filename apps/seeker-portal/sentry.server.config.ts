3f7b39d1d54dc521adc308cd1a7a45ec97841ef7
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-06-26 09:35:08 +0000

    increase frontend trace sample rate

--- apps/seeker-portal/sentry.server.config.ts
+++ apps/seeker-portal/sentry.server.config.ts
@@ -9,7 +9,7 @@ Sentry.init({
 	dsn: env.NEXT_PUBLIC_SENTRY_DSN,
 	environment: env.NEXT_PUBLIC_ENVIRONMENT,
 	// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-	tracesSampleRate: 0.1,
+	tracesSampleRate: 1.0,
 
 	// Setting this option to true will print useful information to the console while you're setting up Sentry.
 	debug: false,
