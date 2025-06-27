4e2f480be468fdbff70ce75361514730ffe86fa2
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-06-27 11:15:10 +0000

    add aws ecr lifecycle policy

--- infrastructure/modules/core/elastic_container_registry.tf
+++ infrastructure/modules/core/elastic_container_registry.tf
@@ -26,3 +26,27 @@ resource "aws_ecr_repository_policy" "lambda_ecr_policy" {
     ]
   })
 }
+
+
+resource "aws_ecr_lifecycle_policy" "lambda" {
+  repository = aws_ecr_repository.backend.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 25 images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 25
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
