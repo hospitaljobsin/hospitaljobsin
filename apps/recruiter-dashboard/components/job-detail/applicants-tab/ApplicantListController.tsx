1e19cc5c5e750be64883292d8c4f378f8d686a3f
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-06-29 16:59:43 +0000

    make change status dropdown trigger color consistent

--- apps/recruiter-dashboard/components/job-detail/applicants-tab/ApplicantListController.tsx
+++ apps/recruiter-dashboard/components/job-detail/applicants-tab/ApplicantListController.tsx
@@ -106,7 +106,6 @@ export default function ApplicantListController(
 					<DropdownTrigger>
 						<Button
 							variant="flat"
-							color="primary"
 							size="sm"
 							endContent={<ChevronDown />}
 							isLoading={isInFlight}
