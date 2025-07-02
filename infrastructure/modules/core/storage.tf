8cc4f1a5ec054691911d8404d8ef0905bd6806c3
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-07-02 07:28:07 +0000

    update s3 bucket properties

--- infrastructure/modules/core/storage.tf
+++ infrastructure/modules/core/storage.tf
@@ -15,3 +15,29 @@ resource "aws_s3_bucket_cors_configuration" "this" {
     max_age_seconds = 3000
   }
 }
+
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = aws_s3_bucket.this.bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.this.arn}/*", # Allow access to all objects within the bucket
+      },
+    ],
+  })
+}
