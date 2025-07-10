771cdd08bd8ab53469fc9e317174c416fd6a505f
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-07-10 15:47:15 +0000

    try to remove suspense from profile page

--- apps/seeker-portal/app/(dashboard)/profile/page.tsx
+++ apps/seeker-portal/app/(dashboard)/profile/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 import type { pageProfileQuery } from "@/__generated__/pageProfileQuery.graphql";
-import ProfileViewSkeleton from "@/components/profile/ProfileViewSkeleton";
-import { Suspense } from "react";
 import { loadQuery, useRelayEnvironment } from "react-relay";
 import { graphql } from "relay-runtime";
 import ProfileClientComponent from "./ProfileClientComponent";
@@ -20,9 +18,11 @@ export default function Profile() {
 		{},
 		{ fetchPolicy: "store-or-network", networkCacheConfig: { force: false } },
 	);
-	return (
-		<Suspense fallback={<ProfileViewSkeleton />}>
-			<ProfileClientComponent queryReference={queryReference} />
-		</Suspense>
-	);
+
+	return <ProfileClientComponent queryReference={queryReference} />;
+	// return (
+	// 	<Suspense fallback={<ProfileViewSkeleton />}>
+	// 		<ProfileClientComponent queryReference={queryReference} />
+	// 	</Suspense>
+	// );
 }
