e2c559ccf1f97afca6ef516f5218b3633354bcf9
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-07-11 15:46:57 +0000

    properly load client side page

--- apps/seeker-portal/app/(dashboard)/profile/page.tsx
+++ apps/seeker-portal/app/(dashboard)/profile/page.tsx
@@ -1,11 +1,15 @@
 "use client";
-import ProfileClientComponent from "./ProfileClientComponent";
+import ProfileViewSkeleton from "@/components/profile/ProfileViewSkeleton";
+import dynamic from "next/dynamic";
+
+const ProfileClientComponent = dynamic(
+	() => import("./ProfileClientComponent"),
+	{
+		ssr: false,
+		loading: () => <ProfileViewSkeleton />,
+	},
+);
 
 export default function Profile() {
 	return <ProfileClientComponent />;
-	// return (
-	// 	<Suspense fallback={<ProfileViewSkeleton />}>
-	// 		<ProfileClientComponent queryReference={queryReference} />
-	// 	</Suspense>
-	// );
 }
