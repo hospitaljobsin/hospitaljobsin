f94aa9a3d009189546597825bbadc6d830ab73c4
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-07-12 04:45:18 +0000

    add sst router ordered cache behaviours

--- sst-router/sst.config.ts
+++ sst-router/sst.config.ts
@@ -10,6 +10,7 @@ export default $config({
 		};
 	},
 	async run() {
+		const noCachePolicyId = "c77fd332-13ef-467a-83a6-75fef01eb223";
 		new sst.aws.Router("SharedRouter", {
 			domain: {
 				name: "hospitaljobs.in",
@@ -18,9 +19,27 @@ export default $config({
 			},
 			transform: {
 				cdn(args, opts, name) {
+					args.orderedCacheBehaviors = [
+						{
+							pathPattern: "*.png",
+							targetOriginId: "s3-origin",
+							viewerProtocolPolicy: "redirect-to-https",
+							allowedMethods: ["GET", "HEAD"],
+							cachedMethods: ["GET", "HEAD"],
+							cachePolicyId: "658327ea-f89d-4fab-a63d-7e88639e58f6", // managed-caching-optimized
+						},
+						{
+							pathPattern: "**",
+							targetOriginId: "s3-origin",
+							viewerProtocolPolicy: "redirect-to-https",
+							allowedMethods: ["GET", "HEAD", "OPTIONS"],
+							cachedMethods: ["GET", "HEAD"],
+							cachePolicyId: noCachePolicyId, // Replace with the ID from your transform.cachePolicy config
+						},
+					];
 					args.defaultCacheBehavior = {
 						...args.defaultCacheBehavior,
-						// cachePolicyId: "658327ea-f89d-4fab-a63d-7e88639e58f6", // managed-caching-optimized
+						cachePolicyId: noCachePolicyId, // managed-caching-optimized
 						viewerProtocolPolicy: "redirect-to-https",
 					};
 				},
