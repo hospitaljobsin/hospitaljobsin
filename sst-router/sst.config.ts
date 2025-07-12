56ebc7eda8e647295810468c4731d2870e0242a0
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-07-12 09:08:05 +0000

    redeploy accounts

--- sst-router/sst.config.ts
+++ sst-router/sst.config.ts
@@ -25,47 +25,47 @@ export default $config({
 						// cachePolicyId: "658327ea-f89d-4fab-a63d-7e88639e58f6", // managed-caching-optimized
 						viewerProtocolPolicy: "redirect-to-https",
 					};
-					args.origins = [
-						{
-							originId: "default",
-							domainName: "hospitaljobs.in",
-							customOriginConfig: {
-								originProtocolPolicy: "https-only",
-								httpPort: 80,
-								httpsPort: 443,
-								originSslProtocols: ["TLSv1.2"],
-								originReadTimeout: 30,
-							},
-						},
-					];
-					args.orderedCacheBehaviors = [
-						// Next.js versioned assets
-						{
-							pathPattern: "/_next/static/*",
-							targetOriginId: args.defaultCacheBehavior.targetOriginId,
-							viewerProtocolPolicy: "https-only",
-							allowedMethods: ["GET", "HEAD"],
-							cachedMethods: ["GET", "HEAD"],
-							cachePolicyId: cachingOptimizedPolicyId,
-							originRequestPolicyId: "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf", // Managed-CORS-S3Origin
-							minTtl: 0,
-							defaultTtl: 31536000,
-							maxTtl: 31536000,
-						},
-						// Public assets outside _next
-						{
-							pathPattern: "/assets/*",
-							targetOriginId: args.defaultCacheBehavior.targetOriginId,
-							viewerProtocolPolicy: "https-only",
-							allowedMethods: ["GET", "HEAD"],
-							cachedMethods: ["GET", "HEAD"],
-							cachePolicyId: cachingOptimizedPolicyId,
-							originRequestPolicyId: "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf", // Managed-CORS-S3Origin
-							minTtl: 0,
-							defaultTtl: 31536000,
-							maxTtl: 31536000,
-						},
-					];
+					// args.origins = [
+					// 	{
+					// 		originId: "default",
+					// 		domainName: "hospitaljobs.in",
+					// 		customOriginConfig: {
+					// 			originProtocolPolicy: "https-only",
+					// 			httpPort: 80,
+					// 			httpsPort: 443,
+					// 			originSslProtocols: ["TLSv1.2"],
+					// 			originReadTimeout: 30,
+					// 		},
+					// 	},
+					// ];
+					// args.orderedCacheBehaviors = [
+					// 	// Next.js versioned assets
+					// 	{
+					// 		pathPattern: "/_next/static/*",
+					// 		targetOriginId: args.defaultCacheBehavior.targetOriginId,
+					// 		viewerProtocolPolicy: "https-only",
+					// 		allowedMethods: ["GET", "HEAD"],
+					// 		cachedMethods: ["GET", "HEAD"],
+					// 		cachePolicyId: cachingOptimizedPolicyId,
+					// 		originRequestPolicyId: "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf", // Managed-CORS-S3Origin
+					// 		minTtl: 0,
+					// 		defaultTtl: 31536000,
+					// 		maxTtl: 31536000,
+					// 	},
+					// 	// Public assets outside _next
+					// 	{
+					// 		pathPattern: "/assets/*",
+					// 		targetOriginId: args.defaultCacheBehavior.targetOriginId,
+					// 		viewerProtocolPolicy: "https-only",
+					// 		allowedMethods: ["GET", "HEAD"],
+					// 		cachedMethods: ["GET", "HEAD"],
+					// 		cachePolicyId: cachingOptimizedPolicyId,
+					// 		originRequestPolicyId: "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf", // Managed-CORS-S3Origin
+					// 		minTtl: 0,
+					// 		defaultTtl: 31536000,
+					// 		maxTtl: 31536000,
+					// 	},
+					// ];
 				},
 				cachePolicy(args, opts, name) {
 					args.name = "NextJsAppRouterPolicy";
@@ -73,7 +73,7 @@ export default $config({
 						"Custom cache policy for Next.js App Router + next/image";
 					args.minTtl = 0;
 					args.defaultTtl = 0;
-					args.maxTtl = 1; // Must be > 0 to not disable caching completely
+					args.maxTtl = 31536000; // Must be > 0 to not disable caching completely
 					args.parametersInCacheKeyAndForwardedToOrigin = {
 						enableAcceptEncodingGzip: true,
 						enableAcceptEncodingBrotli: true,
