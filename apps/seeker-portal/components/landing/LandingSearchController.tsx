6762f9cc6488c4e6c1fa992872a01d432bf28834
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-07-13 16:37:36 +0000

    use textract start document analysis

--- apps/seeker-portal/components/landing/LandingSearchController.tsx
+++ apps/seeker-portal/components/landing/LandingSearchController.tsx
@@ -46,7 +46,6 @@ export function LandingSearchController() {
 						onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
 							setSearchTerm(e.target.value)
 						}
-						isRequired
 						isClearable
 						onClear={() => setSearchTerm("")}
 					/>
