16099b541d7eba4b965fdb0f39968d4bc9f6ec24
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-07-16 16:13:09 +0000

    update org detail page: highlight org description

--- apps/seeker-portal/components/organization-detail/overview-tab/OrganizationDetails.tsx
+++ apps/seeker-portal/components/organization-detail/overview-tab/OrganizationDetails.tsx
@@ -1,6 +1,6 @@
 import type { OrganizationDetailsFragment$key } from "@/__generated__/OrganizationDetailsFragment.graphql";
 import type { OrganizationDetailsInternalFragment$key as OrganizationDetailsInternalFragmentType } from "@/__generated__/OrganizationDetailsInternalFragment.graphql";
-import { Card, CardBody, CardHeader, Link } from "@heroui/react";
+import { Card, CardBody, CardHeader, Divider, Link } from "@heroui/react";
 import { Mail, MapPin } from "lucide-react";
 import Image from "next/image";
 import { graphql, useFragment } from "react-relay";
@@ -52,22 +52,46 @@ export default function OrganizationDetails({
 			<Card fullWidth className="p-6 space-y-6" shadow="none">
 				<CardHeader>
 					<div className="flex w-full justify-between gap-12 sm:gap-6 sm:items-center flex-col sm:flex-row items-start">
-						<div className="flex gap-6 items-start w-full">
-							<div className="relative aspect-square w-16 h-16">
-								<Image
-									src={data.logoUrl}
-									alt={data.name}
-									fill
-									className="rounded-md object-cover"
-								/>
-							</div>
-							<div className="flex flex-col gap-3 items-start w-full">
-								<h4 className="text-lg sm:text-xl font-medium">{data.name}</h4>
-								{data.description && (
-									<p className="text-sm sm:text-md font-normal text-foreground-500">
-										{data.description}
-									</p>
-								)}
+						<div className="w-full flex flex-col gap-6">
+							<div className="flex flex-col sm:flex-row gap-6 items-start w-full">
+								<div className="relative aspect-square w-16 h-16">
+									<Image
+										src={data.logoUrl}
+										alt={data.name}
+										fill
+										className="rounded-md object-cover"
+									/>
+								</div>
+								<div className="flex flex-col gap-4 items-start w-full">
+									<h4 className="text-lg sm:text-xl font-medium">
+										{data.name}
+									</h4>
+									{(data.location || data.email || data.website) && (
+										<div className="flex flex-wrap gap-4 items-center text-foreground-600 w-full">
+											{data.location && (
+												<div className="flex items-center gap-2">
+													<MapPin size={16} />
+													{data.location}
+												</div>
+											)}
+											{data.email && (
+												<div className="flex items-center gap-2">
+													<Mail size={16} /> {data.email}
+												</div>
+											)}
+											{data.website && (
+												<Link
+													showAnchorIcon
+													className="text-foreground-600"
+													href={data.website}
+													isExternal
+												>
+													Visit website
+												</Link>
+											)}
+										</div>
+									)}
+								</div>
 							</div>
 						</div>
 						{/* TODO: add this after notifications infra
@@ -76,34 +100,20 @@ export default function OrganizationDetails({
 						</Button>*/}
 					</div>
 				</CardHeader>
-				{(data.location || data.email || data.website) && (
-					<CardBody className="flex flex-col gap-6 w-full">
-						<div className="flex flex-wrap gap-8 items-center text-foreground-600 w-full">
-							{data.location && (
-								<div className="flex items-center gap-2">
-									<MapPin size={16} />
-									{data.location}
-								</div>
-							)}
-							{data.email && (
-								<div className="flex items-center gap-2">
-									<Mail size={16} /> {data.email}
-								</div>
-							)}
-							{data.website && (
-								<Link
-									showAnchorIcon
-									className="text-foreground-600"
-									href={data.website}
-									isExternal
-								>
-									Visit website
-								</Link>
-							)}
-						</div>
-					</CardBody>
-				)}
 			</Card>
+			{data.description && (
+				<Card fullWidth className="p-6" shadow="none">
+					<CardBody className="flex flex-col gap-6 w-full">
+						<h2 className="text-medium font-medium text-foreground-600">
+							About the Organization
+						</h2>
+						<Divider />
+						<p className="text-sm sm:text-md font-normal text-foreground-500">
+							{data.description}
+						</p>
+					</CardBody>
+				</Card>
+			)}
 		</div>
 	);
 }
