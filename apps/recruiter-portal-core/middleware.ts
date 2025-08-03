1c87cd4dd9613fb873768ce10ee6848ab2c03d9c
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-08-03 04:36:19 +0000

    modify csp header to support posthog

--- apps/recruiter-portal-core/middleware.ts
+++ apps/recruiter-portal-core/middleware.ts
@@ -34,10 +34,12 @@ export async function middleware(request: NextRequest) {
 	const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 	const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
-    img-src 'self' blob: data:;
-    font-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://*.posthog.com;
+    style-src 'self' 'nonce-${nonce}' https://*.posthog.com;
+    img-src 'self' blob: data: https://*.posthog.com;
+    font-src 'self' https://*.posthog.com;
+    connect-src 'self' https://*.posthog.com;
+    worker-src 'self' blob: data:;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
