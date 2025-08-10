ceb08bcfa0d1eb034937c9ab1abd04f7eaa48b47
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-08-10 13:59:30 +0000

    add e2e tests TODO note

--- e2e/tests/global.teardown.ts
+++ e2e/tests/global.teardown.ts
@@ -2,6 +2,8 @@ import { execSync } from "node:child_process";
 
 async function globalTeardown() {
 	console.log("🧹 Tearing down test data...");
+	// TODO: make an api call that tears down data, with secured credentials
+	// OR trigger AWS Lambda function that tears down data
 	execSync("docker compose exec test-server python scripts/teardown_e2e.py", {
 		stdio: "inherit",
 	});
