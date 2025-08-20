c56a4df13c0c38d1c3a022a787982efd6a17fbdb
Aryan Iyappan <aryaniyappan2006@gmail.com>
2025-08-20 16:50:05 +0000

    update env vars

--- infrastructure/modules/aws-core/lambda.tf
+++ infrastructure/modules/aws-core/lambda.tf
@@ -538,6 +538,7 @@ resource "aws_lambda_function" "automation_generate_wa_messages" {
       SERVER_DATABASE_URL          = "${var.mongodb_connection_string}?authMechanism=MONGODB-AWS&authSource=$external"
       SERVER_DEFAULT_DATABASE_NAME = var.mongodb_database_name
       SERVER_LOG_LEVEL             = "DEBUG"
+      SERVER_DEBUG                 = "True"
       SERVER_ENVIRONMENT           = "production"
       SERVER_EMAIl_PROVIDER        = "aws_ses"
       SERVER_EMAIL_FROM            = aws_ses_email_identity.this.email
